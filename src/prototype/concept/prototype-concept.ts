88c3fa8fc24c4bef18f39d25988e3ba299ebeff3
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-07-19 18:35:37 +0300

    Prototype pattern

---
+++ src/prototype/concept/prototype-concept.ts
@@ -0,0 +1,36 @@
+interface IPrototype {
+  // interface with clone method
+  clone(): this;
+  // The clone is deep or shallow
+  // It is up to you how you want to implement
+  // the details in your concrete class
+}
+
+class MyClass implements IPrototype {
+  // A Concrete Class
+  fields: number[];
+
+  constructor(fields: number[]) {
+    this.fields = fields; // any value of any type
+  }
+
+  clone() {
+    return Object.assign({}, this); // shallow copy
+    // return JSON.parse(JSON.stringify(this)); // deep copy
+  }
+}
+
+// The Client
+const OBJECT1 = new MyClass([1, 2, 3, 4]); // Create an object containing an array
+console.log(`OBJECT1: ${JSON.stringify(OBJECT1)}`);
+
+const OBJECT2 = OBJECT1.clone(); // Clone
+console.log(`OBJECT2: ${JSON.stringify(OBJECT2)}`);
+// Change the value of one of the array elements in OBJECT2
+// Depending on your clone method, either a shallow or deep copy
+// was performed
+OBJECT2.fields[1] = 101;
+
+// Comparing OBJECT1 and OBJECT2
+console.log(`OBJECT2: ${JSON.stringify(OBJECT2)}`);
+console.log(`OBJECT1: ${JSON.stringify(OBJECT1)}`);
